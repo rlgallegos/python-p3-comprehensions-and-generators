5a075ecc4be340961720eff0fa2ce38546926976
Robert <rlgallegos85@gmail.com>
2023-03-13 12:15:54 -0400

    Finished List Comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    events_list = [n for n in num_list if n % 2 == 0]
+    return events_list
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = [n + "!" for n in sentence_list]
+    return exclamation_list
